ca00a7ebc8ba949a8e3f3819c3d4c199abb19bb5
PC <PC@MSI-PC97>
2024-03-18 17:38:00 +0300

    fancshin

---
+++ Arrays/main.cpp
@@ -0,0 +1,72 @@
+﻿#include<iostream>
+using namespace std;
+
+#define tab "\t"
+int FillRand(int minRand, int maxRand);//заполняем массив случайными числами 
+void Print(int arr[], const int n);
+void Sum(int arr[], const int n);
+void Avg(int arr[], const int n);
+void minValueIn(int arr[], const int n);
+void maxValueIn(int arr[], const int n);
+
+void main() {
+	setlocale(LC_ALL, "");
+	const int n = 5;
+	int arr[n];
+	int minRand, maxRand;
+	do
+	{
+		cout << "Введите Минимально возможное случайное число: "; cin >> minRand;
+		cout << "Введите Максимально возможное случайное число: "; cin >> maxRand;
+		if (minRand == maxRand)cout << "пределы диапазона не должны совпадать" << endl;
+	} while (minRand==maxRand);
+	if (maxRand < minRand) {
+		int buffer = minRand;
+		minRand = maxRand;
+		maxRand = buffer;
+	}
+	for (int i = 0; i < n; i++) {
+		arr[i] = FillRand(minRand, maxRand);
+	}
+	Print(arr, n);
+	cout << endl;
+	Sum(arr, n);
+	Avg(arr,n);
+	minValueIn(arr, n);
+	maxValueIn(arr, n);
+}
+int FillRand(int minRand, int maxRand) {
+	return minRand + rand() % (maxRand - minRand);
+}
+void Print(int arr[], const int n) {
+	 for (int i = 0; i < n; i++) {
+		 cout << arr[i] << tab;
+	}
+}
+void Sum(int arr[], const int n) {
+	int sum = 0;
+	for (int i = 0; i < n; i++) {
+		 sum += arr[i];
+	}
+	cout << "сума масива = " << sum << endl;
+}
+void Avg(int arr[], const int n) {
+	int sum = 0;
+	for (int i = 0; i < n; i++) {
+		sum += arr[i];
+	}cout << "Средне арефметическое = " << (double)sum / n << endl;
+}
+void minValueIn(int arr[], const int n){
+	int Min = arr[0];
+	for (int i = 0; i < n; i++) {
+		if (arr[i] < Min) Min = arr[i];
+	}
+	cout << "Минемальный елемент = " << Min << endl;
+}
+void maxValueIn(int arr[], const int n) {
+	int Max=arr[0];
+	for (int i = 0; i < n; i++) {
+		if(arr[i] > Max) Max = arr[i];
+	}
+	cout << "Максимальный елемент = " << Max << endl;
+}
--- Functions/main.cpp
+++ Functions/main.cpp
@@ -1,41 +1,73 @@
 ﻿#include<iostream>
+#include<Windows.h>
 using namespace std;
 
-int Sum(int a, int b);
+int Sum(int a, int b,int c=0);
 int Diff(int a, int b);
 int Prod(int a, int b);
 double Quote(int a, int b);
 double Faktorial1(int a);
 double Faktorial2(int b);
 double Stepen(int a, int b);
+double Power(double a, int n);
+//#define POWER
+//#define CALC
+//#define FACKTORIAL
 void main() {
 	setlocale(LC_ALL, "");
+#ifdef CALC
 	int a, b;
 	cout << "введите два числа: "; cin >> a >> b;
-	int c = Sum(a, b);
+	int c = Sum(a, b);//Использувание функции (Вызов  функции - Function call)
 	cout << a << " + " << b << " = " << c << endl;
-	cout << a << " - " << b << " = " <<Diff(a,b) << endl;
-	cout << a << " * " << b << " = " <<Prod(a,b) << endl;
-	cout << a << " / " << b << " = " <<Quote(a,b) << endl;
+	cout << a << " - " << b << " = " << Diff(a, b) << endl;
+	cout << a << " * " << b << " = " << Prod(a, b) << endl;
+	cout << a << " / " << b << " = " << Quote(a, b) << endl;
+#endif // CALC
+
+#ifdef FACKTORIAL
+	int a, b;
 	cout << "Фактрориал числа " << a << "\n" << Faktorial1(a) << endl;
 	cout << "Фактрориал числа " << b << "\n" << Faktorial2(b) << endl;
-	cout << "Возводим число " << a << "в степень" << b << "\n" << "получаем" << "\n" << Stepen(a, b) << endl;
+#endif // 
+#ifdef POWER
+	int a;
+	int n;
+	cout << "Ведите основания степени: "; cin >> a;
+	cout << "Ведите показание степени: "; cin >> n;
+	cout << a << " ^ " << n << " = " << Power(a, n) << endl;
+	system("PAUSE");
+	main();
+#endif // POWER
+
+
+	//cout << "Возводим число " << a << " в степень " << b << "\n" << "получаем " << "\n" << Stepen(a, b) << endl;
+}
+double Power(double a, int n) {
+	double N = 1;
+	if (n < 0) {
+		a = 1 / a;
+		n = -n;
+	}
+	for (int i = 0; i < n; i++) {
+		N *= a;
+	}
+	return N;
 }
 double Stepen(int a, int b) {
-	double STep=1;//a ----- число возводимое в с тепень
+	double STep = 1;//a ----- число возводимое в с тепень
 	//b ------ степень в которую возводим чесло
 	if (b < 0) {
 		a = 1 / a;
 		b = -b;
 	}
-	int i = 0;
-	for (i; i < b; i++) {
+	for (int i = 1; i < b; i++) {
 		STep *= a;
 	}return STep;
 }
 int Sum(int a, int b) {
-	int c = a + b;
-	return c;
+	int d = a + b;
+	return d;
 }
 int Diff(int a, int b) {
 	return a - b;
@@ -47,15 +79,13 @@ double Quote(int a, int b) {
 	return (double)a / b;
 }
 double Faktorial1(int a) {
-	int i = 1;
-	double factarial = 1; for (i; i <= a; i++)
+	double factarial = 1; for (int i = 1; i <= a; i++)
 	{
 		factarial *= i;
 	}return factarial;
 }
 double Faktorial2(int b) {
-	int i = 1;
-	double factarial = 1; for (i; i <= b; i++)
+	double factarial = 1; for (int i = 1; i <= b; i++)
 	{
 		factarial *= i;
 	}return factarial;
