f983dc1d48a70c3e8bba121a63a5cd67ded95aeb
PC <PC@MSI-PC97>
2024-04-19 18:13:54 +0300

    Shablon

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -1,38 +1,107 @@
 ﻿#include<iostream>
 using namespace std;
-
 #define tab "\t"
+#define delimiter "\n-----------------------------------------------------\n"
+
+const int ROWS = 3;	//количество строк
+const int COLS = 4; //количество элементов строки
+
+
 void FillRand(int arr[], const int n, int minRand = 0, int maxRand = 100);//заполняем массив случайными числами 
 void FillRand(double arr[], const int n, int minRand = 0, int maxRand = 100);//заполняем массив случайными числами 
+
+void FillRand(int arr[ROWS][COLS], const int ROWS, const int COLS, int minRand = 0, int maxRand = 100);
 
 template<typename T>
 void Print(const T arr[], const int n);
 template<typename T>
-T Sum(const T arr[], const int n);
-int AVG(const int arr[], const int n);
+void Print(const T arr[ROWS][COLS], const int ROWS, const int COLS);
 
+template<typename T>
+T Sum(const T arr[], const int n);
+template<typename T>
+T  Sum(const T arr[ROWS][COLS], const int ROWS, const int COLS);
+template<typename T>
+T AVG(const T arr[], const int n);
+template<typename T>
+T AVG(const T arr[ROWS][COLS], const int ROWS, const int COLS);
+template<typename T>
+T minValueIn(const T arr[], const int n);
+template<typename T>
+T minValueIn(const T arr[ROWS][COLS], const int ROWS, const int COLS);
+template<typename T>
+T maxValueIn(const T arr[], const int n);
+template<typename T>
+T maxValueIn(const T arr[ROWS][COLS], const int ROWS, const int COLS);
+template<typename T>
+void shiftLeft(T arr[], const int n, const int number_of_shifts);
+template<typename T>
+void shiftLeft(T arr[ROWS][COLS], const int ROWS, const int COLS, int number_of_shifts);
+template<typename T>
+void shiftRight(T arr[], const int n, const int number_of_shifts);
+template<typename T>
+void shiftRight(T arr[ROWS][COLS], const int ROWS, const int COLS, int number_of_shifts);
+template<typename T>
+void Sort(T arr[], const int n);
+template<typename T>
+void Sort(T arr[ROWS][COLS], const int ROWS, const int COLS);
+template<typename T>
+void Unique(T arr[], const int n);
+template<typename T>
+void Unique(T arr[ROWS][COLS], const int ROWS, const int COLS);
 
 void main() {
 	setlocale(LC_ALL, "");
-	const int n = 5;
-	int arr[n];
-	int minRand, maxRand;
-	///*do//{
-	//	cout << "Введите Минимально возможное случайное число: "; cin >> minRand;
-	//	cout << "Введите Максимально возможное случайное число: "; cin >> maxRand;
-	//	if (minRand == maxRand)cout << "пределы диапазона не должны совпадать" << endl;
-	//} while (minRand==maxRand);*/
-	FillRand(arr, n);
-	Print(arr, n);
-	cout << "Сума елементов масива: " << Sum(arr, n) << endl;
-	cout << "Средне арефметическое элементов масива: " << AVG(arr, n) << endl;
+	const int I_SIZE = 10;
+	int i_arr[I_SIZE];
+	//i_arr[-1] = 0;
 
+	/*int minRand, maxRand;
+	do
+	{
+		cout << "Введите минимально возможное случайное число: "; cin >> minRand;
+		cout << "Введите максимально возможное случайное число: "; cin >> maxRand;
+		if (minRand == maxRand)cout << "Пределы диапазона не должны совпадать" << endl;
+	} while (minRand == maxRand);*/
+
+	Unique(i_arr, I_SIZE);
+	Print(i_arr, I_SIZE);
+	cout << "Сумма элементов массива: " << Sum(i_arr, I_SIZE) << endl;
+	cout << "Среднее-арифметическое элементов массива: " << AVG(i_arr, I_SIZE) << endl;
+	cout << "Минимальное значение в массиве: " << minValueIn(i_arr, I_SIZE) << endl;
+	cout << "Максимальное значение в массиве: " << maxValueIn(i_arr, I_SIZE) << endl;
+	Sort(i_arr, I_SIZE);
+	Print(i_arr, I_SIZE);
 	int number_of_shifts;
-	const int m = 8;
-	double brr[m];
-	FillRand(brr, m);
-	Print(brr, m);
-	cout << "Сума елементов масивоа: " << Sum(brr, m) << endl;
+	cout << "Введите количество сдвигов: "; cin >> number_of_shifts;
+	shiftRight(i_arr, I_SIZE, number_of_shifts);
+	Print(i_arr, I_SIZE);
+
+	cout << delimiter << endl;
+
+	const int D_SIZE = 8;
+	double d_arr[D_SIZE];
+	FillRand(d_arr, D_SIZE);
+	Print(d_arr, D_SIZE);
+
+	cout << delimiter << endl;
+
+	int i_arr_2[ROWS][COLS];
+	Unique(i_arr_2, ROWS, COLS);
+	//FillRand(i_arr_2, ROWS, COLS);
+	Print(i_arr_2, ROWS, COLS);
+	cout << "Сумма элементов массива: " << Sum(i_arr_2, ROWS, COLS) << endl;
+	cout << "Среднее-арифметическое элементов массива: " << AVG(i_arr_2, ROWS, COLS) << endl;
+	cout << "Минимальное значение в массиве: " << minValueIn(i_arr_2, ROWS, COLS) << endl;
+	cout << "Максимальное значение в массиве:" << maxValueIn(i_arr_2, ROWS, COLS) << endl;
+	cout << "Введите количество сдвигов: "; cin >> number_of_shifts;
+	shiftRight(i_arr_2, ROWS, COLS, number_of_shifts);
+	Print(i_arr_2, ROWS, COLS);
+	Sort(i_arr_2, ROWS, COLS);
+	Print(i_arr_2, ROWS, COLS);
+
+	cout << delimiter << endl;
+
 }
 void FillRand(int arr[], const int n, int minRand, int maxRand) {
 	if (maxRand < minRand) {
@@ -58,12 +127,36 @@ void FillRand(double arr[], const int n, int minRand, int maxRand)
 		arr[i] /= 100;
 	}
 }
+void FillRand(int arr[ROWS][COLS], const int ROWS, const int COLS, int minRand, int maxRand)
+{
+	//Заполнение двумерного массива случайными числами:
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			arr[i][j] = rand() % 100;
+		}
+	}
+}
 template<typename T>
 void Print(const T arr[], const int n) {
 	for (int i = 0; i < n; i++){
 		cout << arr[i] << tab;
 	}
 	cout << endl;
+}
+template<typename T>
+void Print(const T arr[ROWS][COLS], const int ROWS, const int COLS)
+{
+	//Вывод двумерного массива на экран:
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			cout << arr[i][j] << tab;
+		}
+		cout << endl;
+	}
 }
 template<typename T>
 T Sum(const T arr[], const int n) {
@@ -73,7 +166,234 @@ T Sum(const T arr[], const int n) {
 	}
 	return sum;
 }
-
-int AVG(const int arr[], const int n) {
+template<typename T>
+T  Sum(const T arr[ROWS][COLS], const int ROWS, const int COLS)
+{
+	T sum = 0;
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			sum += arr[i][j];
+		}
+	}
+	return sum;
+}
+template<typename T>
+T AVG(const T arr[], const int n) {
 	return (double)Sum(arr, n) / n;
+}
+template<typename T>
+T AVG(const T arr[ROWS][COLS], const int ROWS, const int COLS)
+{
+	return (double)Sum(arr, ROWS, COLS) / (ROWS * COLS);
+}
+template<typename T>
+T minValueIn(const T arr[], const int n)
+{
+	T min = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] < min)min = arr[i];
+	}
+	return min;
+}
+template<typename T>
+T minValueIn(const T arr[ROWS][COLS], const int ROWS, const int COLS)
+{
+	T min = arr[0][0];
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			if (arr[i][j] < min)min = arr[i][j];
+		}
+	}
+	return min;
+}
+template<typename T>
+T maxValueIn(const T arr[], const int n)
+{
+	T max = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] > max)max = arr[i];
+	}
+	return max;
+}
+template<typename T>
+T maxValueIn(const T arr[ROWS][COLS], const int ROWS, const int COLS)
+{
+	T max = arr[0][0];
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			if (arr[i][j] > max)max = arr[i][j];
+		}
+	}
+	return max;
+}
+template<typename T>
+void shiftLeft(T arr[], const int n, const int number_of_shifts)
+{
+	for (int i = 0; i < number_of_shifts; i++)
+	{
+		int buffer = arr[0];
+		for (int i = 1; i < n; i++)
+		{
+			arr[i - 1] = arr[i];
+		}
+		arr[n - 1] = buffer;
+	}
+}
+template<typename T>
+void shiftLeft(T arr[ROWS][COLS], const int ROWS, const int COLS, int number_of_shifts)
+{
+	/*for (int i = 0; i < ROWS; i++)
+	{
+		shiftLeft(arr[i], COLS, number_of_shifts);
+	}*/
+	for (int i = 0; i < number_of_shifts; i++)
+	{
+		T buffer = arr[0][0];
+		for (int i = 0; i < ROWS; i++)
+		{
+			for (int j = 1; j < COLS; j++)
+			{
+				arr[i][j - 1] = arr[i][j];
+			}
+			if (i < ROWS - 1)arr[i][COLS - 1] = arr[i + 1][0];
+		}
+		arr[ROWS - 1][COLS - 1] = buffer;
+	}
+}
+template<typename T>
+void shiftRight(T arr[], const int n, const int number_of_shifts)
+{
+	 shiftLeft(arr, n, n - number_of_shifts % n);
+}
+template<typename T>
+void shiftRight(T arr[ROWS][COLS], const int ROWS, const int COLS, int number_of_shifts)
+{
+	shiftLeft(arr, ROWS, COLS, ROWS * COLS - number_of_shifts);
+}
+template<typename T>
+void Sort(T arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+		//счетчик 'i' выбирает элемент массива, 
+		//в который будет помощено минимальное значение из всех перебираемых.
+	{
+		for (int j = i + 1; j < n; j++)
+			//счетчик 'j' перебирает оставшиеся элементы массива.
+		{
+			//arr[i] - выбранный элемент.
+			//arr[j] - перебираемый элемент.
+			if (arr[j] < arr[i])
+			{
+				T buffer = arr[i];
+				arr[i] = arr[j];
+				arr[j] = buffer;
+			}
+		}
+	}
+}
+template<typename T>
+void Sort(T arr[ROWS][COLS], const int ROWS, const int COLS)
+{
+	T iterations = 0;
+	T exchanges = 0;
+	//Эти два цикла выбирают элемент:
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			/*for (int k = j + 1; k < COLS; k++)
+			{
+				//arr[i][j] - выбранный элемент
+				//arr[i][k] - перебираемый элемент
+				iterations++;
+				if (arr[i][k] < arr[i][j])
+				{
+					int buffer = arr[i][j];
+					arr[i][j] = arr[i][k];
+					arr[i][k] = buffer;
+					exchanges++;
+				}
+			}*/
+			//Эти два цикла перебираеют оставшиеся элементы в поиске минимального значения:
+			for (int k = i; k < ROWS; k++)
+			{
+				for (int l = k == i ? j + 1 : 0; l < COLS; l++)
+				{
+					//if (k == i && l == 0)l = j + 1;
+					//if (l == COLS)break;
+					//arr[i][j] - выбранный элемент
+					//arr[k][l] - перебираемый элемент
+					iterations++;
+					if (arr[i][j] > arr[k][l])
+					{
+						int buffer = arr[i][j];
+						arr[i][j] = arr[k][l];
+						arr[k][l] = buffer;
+						exchanges++;
+					}
+				}
+			}
+		}
+	}
+	cout << "Массив отсортирован за " << iterations << " итераций.\n";
+	cout << "При этом было выполнено " << exchanges << " обменов элементов.\n";
+}
+template<typename T>
+void Unique(T arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		bool unique;
+		do
+		{
+			arr[i] = rand() % n;
+			unique = true;	//предполагаем, что сгенерировалось уникальное случайное число,
+			//но это нужно проветрить:
+			for (int j = 0; j < i; j++)
+			{
+				if (arr[i] == arr[j])
+				{
+					unique = false;
+					break;
+				}
+			}
+		} while (!unique);
+	}
+}
+template<typename T>
+void Unique(T arr[ROWS][COLS], const int ROWS, const int COLS)
+{
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			bool unique;
+			do
+			{
+				arr[i][j] = rand() % (ROWS * COLS);
+				unique = true;	//преположим, что сгенерировалось униакльное случайное число,
+				//но это нужно проветрить:
+				for (int k = 0; k <= i; k++)
+				{
+					for (int l = 0; l < (k == i ? j : COLS); l++)
+					{
+						if (arr[i][j] == arr[k][l])
+						{
+							unique = false;
+							break;
+						}
+					}
+					if (!unique)break;
+				}
+			} while (!unique);
+		}
+	}
 }
